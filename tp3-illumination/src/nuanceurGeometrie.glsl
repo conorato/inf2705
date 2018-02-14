e8b31c74e040c94d2d8a4410f1ec124c247c5b94
William Harvey <wihar@l4712-15.info.polymtl.ca>
2018-02-14 14:09:30 -0500

    C0-WH: Ajout fichiers tp3

---
+++ tp3-illumination/src/nuanceurGeometrie.glsl
@@ -0,0 +1,40 @@
+#version 410
+
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 3) out;
+
+uniform mat4 matrModel;
+uniform mat4 matrVisu;
+uniform mat4 matrProj;
+
+layout (std140) uniform varsUnif
+{
+   // partie 1: illumination
+   int typeIllumination;     // 0:Lambert, 1:Gouraud, 2:Phong
+   bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
+   bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
+   bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
+   // partie 3: texture
+   int texnumero;            // numéro de la texture appliquée
+   bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
+   int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
+};
+
+in Attribs {
+   vec4 couleur;
+} AttribsIn[];
+
+out Attribs {
+   vec4 couleur;
+} AttribsOut;
+
+void main()
+{
+   // émettre les sommets
+   for ( int i = 0 ; i < gl_in.length() ; ++i )
+   {
+      gl_Position = gl_in[i].gl_Position;
+      AttribsOut.couleur = AttribsIn[i].couleur;
+      EmitVertex();
+   }
+}
